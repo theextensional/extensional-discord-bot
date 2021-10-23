cdd070cb2c3566c7532539164c48238782b2d9e2
bezvozni <30541791+bezvozni@users.noreply.github.com>
2021-10-23 18:23:13 +0300

    config.py добавлен в индекс git

---
+++ config.py
@@ -0,0 +1,14 @@
+"""Configuration."""
+TOKEN = 'OTAxNDM1NTY2NDA1NTk5MjY0.YXP1Sg.07HV4fuXK8f1DUEiI6nNwknBnsg'
+PREFIX = '+'
+DB_NAME = 'bot.db'
+# QDAY_CHANNEL_ID = int
+# CH_BUMP = int
+# ROLE_BUMP = int
+# VOICE_TRIGGER = [int, int]
+# VK_API_TOKEN = str
+# VK_API_VERSION = float
+# VK_DOMAIN = str
+# VK_GROUP_ID = int
+# VK_DATA_FILE = str  # example './data/vk_last_post.json'
+# CH_VK_POSTS = int

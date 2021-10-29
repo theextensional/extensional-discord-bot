459187831faedcf99d1254c5e0d346eca8a17d64
bezvozni <30541791+bezvozni@users.noreply.github.com>
2021-10-29 22:52:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ DB_NAME = 'bot.db'
 # CH_BUMP = int
 # ROLE_BUMP = int
 VOICE_TRIGGER = os.environ.get("VOICE_TRIGGER").split(",")
+print(VOICE_TRIGGER)
 sys.stdout.flush()
 # VK_API_TOKEN = str
 # VK_API_VERSION = float

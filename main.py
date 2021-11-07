d585ba80f24af189fe24d37bc5ad7393463a2d50
redboo <garda1@mail.ru>
2021-11-07 14:10:49 +0500

    Revert "адаптация кода для хостинга в heroku (#15)"
    This reverts commit 09bf0fdcba4ba14cf3feb425de7978b6a1aeb4ae.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import config
 import discord
 import sqlite3
 import os
-import sys  # sys для вывода лога бота в консоль heroku
 
 bot = commands.Bot(command_prefix=config.PREFIX,
                    intents=discord.Intents.all())
@@ -81,4 +80,3 @@ async def change_status():
     await bot.change_presence(activity=discord.Game(next(status)))
 
 bot.run(config.TOKEN)
-sys.stdout.flush()
